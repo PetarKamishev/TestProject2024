bbedfffcc16b40e7dd8133abcaea49f028429c3e
PetarKamishev <152514467+PetarKamishev@users.noreply.github.com>
2024-04-30 18:52:14 +0300

    Add files via upload

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -12,7 +12,7 @@ namespace TestProject2024
         public void Setup()
         {
             _handler = new HttpClientHelper();
-            var name = new Guid(Guid.NewGuid().ToString());
+            name = Guid.NewGuid().ToString();
         }
 
         [Test]
@@ -31,6 +31,52 @@ namespace TestProject2024
             var response = await _handler.POST("users", requestBody);
             
             Assert.AreEqual("Created", response.StatusCode.ToString());
-        }     
+        }
+
+        [Test]
+
+        public async Task GetSpecificUser()
+        {
+            var requestBody = _handler.CreateUserRequestBody(name);
+            var request = await _handler.POST("users", requestBody);
+            var responseBody = request.Content.ReadAsStringAsync();
+            var user = JsonConvert.DeserializeObject<UserResponse>(responseBody.Result);
+            var response = await _handler.GET("users", user.Id);
+
+            Assert.AreEqual("OK", response.StatusCode.ToString());
+            Assert.AreEqual(name, user.Name);
+            Assert.IsNotEmpty(user.Id.ToString());
+            Assert.IsNotNull(user.Id);
+            
+        }
+
+        [Test]
+
+        public async Task UpdateUser()
+        {
+            var requestBody = _handler.CreateUserRequestBody(name);
+            var request = await _handler.POST("users", requestBody);
+            var responseBody = request.Content.ReadAsStringAsync();
+            var user = JsonConvert.DeserializeObject<UserResponse>(responseBody.Result);
+            var response = await _handler.UPDATE("users", user.Id, requestBody);
+
+            Assert.AreEqual("OK", response.StatusCode.ToString());
+            Assert.AreEqual(name, user.Name);
+            Assert.IsNotEmpty(user.Id.ToString());
+            Assert.IsNotNull(user.Id);
+        }
+
+        [Test]
+
+        public async Task DeleteUser()
+        {
+            var requestBody = _handler.CreateUserRequestBody(name);
+            var request = await _handler.POST("users", requestBody);
+            var responseBody = request.Content.ReadAsStringAsync();
+            var user = JsonConvert.DeserializeObject<UserResponse>(responseBody.Result);
+            var response = await _handler.DELETE("users", user.Id);
+
+            Assert.AreEqual("NoContent", response.StatusCode.ToString());           
+        }
     }
 }
---
+++ UserRequest.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject2024.Models
+{
+    public class UserRequest
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("gender")]
+        public string Gender { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; } 
+    }
+}
